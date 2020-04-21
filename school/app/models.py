8ebdd83c67c74c2321ebdcc02131fd311cb1af5c
criptik <tom.deneau@gmail.com>
2020-04-21 17:42:31 -0500

    first cut test cases for student-class-teacher.  Need to change to use asserts

--- school/app/models.py
+++ school/app/models.py
@@ -4,11 +4,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 from hashlib import md5
+import sys
 
 # for the many-to-many student-class relationship
 studentclassinfo = db.Table('studentclassinfo',
                             db.Column('student_id', db.Integer, db.ForeignKey('student.id')),
                             db.Column('class_id', db.Integer, db.ForeignKey('schoolclass.id')),
+                            db.Column('class_grade', db.Integer),
                             db.PrimaryKeyConstraint('student_id', 'class_id') )
 
 
@@ -33,6 +35,52 @@ class Teacher(Person):
     def infoStr(self):
         return '%s, payGrade=%s' % (super().infoStr(), self.payGrade)
 
+    # get all classes that this teacher teaches
+    def getClasses(self):
+        return self.schoolclasses.all()
+
+    # get all students who take a class from this teacher
+    def getStudents(self):
+        if False:
+            # simple one using a set and no SQL calls
+            studs = set({})
+            for c in self.getClasses():
+                for s in c.students:
+                    studs.add(s)
+            return studs
+        elif False:
+            # try to use SQL join and filter by test against myclassids
+            myclassids = []
+            for c in self.getClasses():
+                myclassids.append(c.id)
+            # print('myclassids:', myclassids)
+            q = (Student.query.join(studentclassinfo, (studentclassinfo.c.student_id == Student.id))
+                 .filter(studentclassinfo.c.class_id.in_(myclassids))
+                 .order_by(Student.name))
+            # print('SQL is', q)
+            return q.all()
+        else:
+            # try without any data structures that are outside the db
+            q = (Student.query.join(studentclassinfo, (studentclassinfo.c.student_id == Student.id))
+                 .join(Schoolclass, (Schoolclass.id == studentclassinfo.c.class_id))
+                 .filter(Schoolclass.teacher_id == self.id)
+                 .order_by(Student.name))
+            # print('SQL is ', q)
+            return q.all()
+
+    # def recordClassGrade(self, clazz, stud, classGrade):
+    #     # get record from studentclassinfo table
+    #     info = studentclassinfo.query().filter(studentclassinfo.c.class_id == clazz.id,
+    #                                            studentclassinfo.c.student_id == stud.id).all()
+    #     print('before classGrade:', info.__dict__)
+    #     if info == []:
+    #         print('Error no such class-student combination registered')
+    #         return
+
+    #     info.class_grade = classGrade
+    #     print('afterbefore classGrade:', info.__dict__)
+        
+        
 class Student(Person):
     __tablename__ = 'student'
     grade = db.Column(db.Integer)
@@ -58,4 +106,15 @@ class Schoolclass(db.Model):
     
     def __repr__(self):
         tname = None if self.classteacher == None else self.classteacher.name
-        return '<%s %s, teacher=%s>' % (self.__class__.__name__, self.subject, tname)
+        return '<%s %s, teacher=%s, id=%d>' % (self.__class__.__name__, self.subject, tname, self.id)
+
+    def get_teacher_id(self):
+        return self.teacher_id
+
+    def student_ids(self):
+        ids = []
+        for s in self.students:
+            ids.append(s.id)
+        return ids
+        
+    
--- school/tests.py
+++ school/tests.py
@@ -2,73 +2,173 @@ from datetime import datetime, timedelta
 import unittest
 from app import app, db
 from app.models import Student, Teacher, Person, Schoolclass
+import sys
+
+class table1(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    col1 = db.Column(db.Integer)
+    col3 = db.Column(db.Integer)
+    # other columns
+    def __repr__(self):
+        return '(%d, %d)' % (self.col1, self.col3)
+
+class table2(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    col2 = db.Column(db.Integer)
+    col3 = db.Column(db.Integer)
+    # other columns
+    def __repr__(self):
+        return '(%d, %d)' % (self.col2, self.col3)
 
 class StudentTeacherModelCase(unittest.TestCase):
     def setUp(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
         db.create_all()
-
+        
     def tearDown(self):
         db.session.remove()
         db.drop_all()
 
-    def test_student_grade(self):
-        s = Student(name='tom', email='tom@gmail.com', id=1, grade=10)
-        print(s)
+    # def test_student_grade(self):
+    #     s = Student(name='tom', email='tom@gmail.com', grade=10)
+    #     db.session.add(s)
+    #     db.session.commit()
+    #     print(s)
         
-    def test_teacher_paygrade(self):
-        t = Teacher(name='ann', email='ann@gmail.com', id=2, payGrade=99)
-        print(t)
+    # def test_teacher_paygrade(self):
+    #     t = Teacher(name='ann', email='ann@gmail.com', payGrade=99)
+    #     db.session.add(t)
+    #     db.session.commit()
+    #     print(t)
 
-    def test_schoolclass(self):
-        t1 = Teacher(name='jane', email='jane@gmail.com', id=2, payGrade=99)
-        t2 = Teacher(name='mary', email='mary@gmail.com', id=3, payGrade=98)
-        db.session.add_all([t1])
+
+    def not_test_tablejoin(self):
+        t1s = []
+        t2s = []
+        for n in range(1,6):
+            t1s.append(table1(col1 = n, col3 = 2*n % 5))
+        db.session.add_all(t1s)
         db.session.commit()
-        c1 = Schoolclass(subject='Algebra', classteacher=t1)
-        c2 = Schoolclass(subject='Geometry', classteacher=t1)
-        c3 = Schoolclass(subject='Trig')
-        c4 = Schoolclass(subject='Geometry', classteacher=t2)
-        db.session.add_all([c1, c2, c3, c4])
-        db.session.commit()
-        # t1 before append
-        self.assertEqual(t1.schoolclasses.all(), [c1, c2])
-        self.assertEqual(c3.classteacher, None)
-        # do append
-        t1.schoolclasses.append(c3)
-        db.session.commit()
-        # test after append
-        self.assertEqual(t1.schoolclasses.all(), [c1, c2, c3])
-        self.assertEqual(c3.classteacher, t1)
-
-        self.assertEqual(Schoolclass.query.all(), [c1, c2, c3, c4])
-        # check Mary classes
-        self.assertEqual(t2.schoolclasses.all(), [c4])
-
-        s1 = Student(name='john', email='john@gmail.com', id=11)
-        s2 = Student(name='gina', email='gina@gmail.com', id=12)
-        s3 = Student(name='pat', email='pat@gmail.com', id=13)
-
-        s1.studclasses.append(c1)
-        s1.studclasses.append(c3)
-        s2.studclasses.append(c1)
-        s2.studclasses.append(c3)
-        s2.studclasses.append(c4)
-        s3.studclasses.append(c4)
+        for n in range(1,6):
+            t2s.append(table2(col2 = 2*n, col3 = 2*n % 10))
+        db.session.add_all(t2s)
         db.session.commit()
         print()
-        n = 1
-        for s in [s1, s2, s3]:
-            print('s%d, (%s) students are %s' % (n, s, s.studclasses.all()))
+        print('t1s', table1.query.all())
+        print('t2s', table2.query.all())
+        
+        q1 = db.session.query(table1.col1, table2.col2).filter(table1.col3 == table2.col3)
+        print('q1 is', q1.statement)
+        print('q1 all is ', q1.all())
+
+        q2 = db.session.query(table1.col1, table2.col2).join(table2, table1.col3 == table2.col3)
+        print('q2 is', q2.statement)
+        print('q2 all is ', q2.all())
+        return
+
+    def showClassStuds(self, n, c):
+        print('Class c%d, (%s) students are: ' % (n, c), end='')
+        for s in c.students:
+            print(s.name, end=' ')
+        print()
+        
+    
+    def test_schoolclass(self):
+        numTeachers = 4
+        teachers = []
+        for n in range(numTeachers):
+            tname = 't%d' % (n)
+            temail = '%s@gmail.com' % (tname)
+            teachers.append(Teacher(name=tname, email=temail, payGrade=90+n))
+        db.session.add_all(teachers)
+        db.session.commit()
+
+        numStuds = 25
+        studs = []
+        for n in range(numStuds):
+            sname = 's%d' % (n)
+            semail = '%s@gmail.com' % (sname)
+            studs.append(Student(name=sname, email=semail))
+        db.session.add_all(studs)
+        db.session.commit()
+
+        numClasses = 5
+        classes = []
+        subjects = ['Algebra', 'Geometry', 'Trig', 'Calculus']
+        for n in range(numClasses):
+            # add last class with no teacher
+            if n < numClasses-1:
+                ct = teachers[n%2]
+            else:
+                ct = None
+            classes.append(Schoolclass(subject=subjects[n%4], classteacher=ct))
+        db.session.add_all(classes)
+        db.session.commit()
+
+        print()
+        # print(teachers[0].getStudents())
+        
+        # t[0] before append
+        self.assertEqual(teachers[0].schoolclasses.all(), [classes[0], classes[2]])
+        self.assertEqual(classes[4].classteacher, None)
+        # do append
+        teachers[0].schoolclasses.append(classes[-1])
+        db.session.commit()
+        # test after append
+        self.assertEqual(teachers[0].schoolclasses.all(), [classes[0], classes[2], classes[4]])
+        self.assertEqual(classes[4].classteacher, teachers[0])
+
+        self.assertEqual(Schoolclass.query.all(), classes)
+        # check teacher[1] classes
+        self.assertEqual(teachers[1].schoolclasses.all(), [classes[1], classes[3]])
+
+        # assign students to classes
+        n = 0
+        for s in studs:
+            if n >= numStuds-2:
+                continue
+            s.studclasses.append(classes[n % (numClasses-1)])
+            print('s%d in c%d' % (n, n  % (numClasses-1)))
+            if n == 1:
+                s.studclasses.append(classes[2])
+                s.studclasses.append(classes[3])
+                print('s1 in c2, c3')
+            n = n + 1
+        db.session.commit()
+
+        print('Before append, ', end='')
+        self.showClassStuds(1, classes[1])
+        
+        # appending s0 to c1
+        classes[1].students.append(studs[0])
+        db.session.commit()
+        print()
+        print('After Append -----------------')
+        n = 0
+        for c in classes:
+            self.showClassStuds(n, classes[n])
             n = n + 1
 
-        c2.students.append(s1)
-        db.session.commit()
-        print('-----------------')
-        n = 1
-        for c in [c1, c2, c3, c4]:
-            print('c%d, (%s) students are %s' % (n, c, c.students))
+        print('+++')
+
+        for t in teachers:
+            print('Student List for %s:' % (t.name), end=' ')
+            for s in t.getStudents():
+                print(s.name, end=' ')
+            print()
+
+        n = 0
+        for s in studs:
+            print('Student %s: classes are %s' % (s.name, s.studclasses.all()))
             n = n + 1
+
+        if False:
+            # record a grade
+            # get first class and first student in that class
+            clazz = teachers[0].getClasses()[0]
+            stud = clazz.students[0]
+            teachers[0].recordClassGrade(clazz, stud, 98)
+            db.session.commit()
         
 if __name__ == '__main__':
     unittest.main(verbosity=2)
